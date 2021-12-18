2c3a6f7ac10fbff27a87bb9a70d99338d48b4726
Bob Paauwe <bpaauwe@yahoo.com>
2021-12-17 16:30:50 -0800

    set SO_REUSEPORT on socket.
    This allows the address/port combination to be used by multiple
    programs.  At least on FreeBSD.

--- nodes/weatherflow.py
+++ nodes/weatherflow.py
@@ -614,6 +614,7 @@ class Controller(udi_interface.Node):
     def udp_data(self):
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
         s.bind(('0.0.0.0', self.Parameters['ListenPort']))
         self.windspeed = 0
         sky_tm = 0
