513063d6e3202d83551698cbd5427c6cd3697b34
Bob Paauwe <bpaauwe@yahoo.com>
2022-02-22 20:27:30 -0800

    Fix error in REST device query.

--- nodes/weatherflow.py
+++ nodes/weatherflow.py
@@ -255,7 +255,7 @@ class Controller(udi_interface.Node):
                     return None
 
         node = self.poly.getNode(device_id)
-        node.update(jdata['obs'])
+        node.update(jdata['obs'], False)
 
     def create_device_node(self, station, device, units, elevation):
         """
