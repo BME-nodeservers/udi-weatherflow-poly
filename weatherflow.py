d8c8c9cde4d5642428c0b76ef5706004386a2d7e
Bob Paauwe <bpaauwe@yahoo.com>
2022-11-21 10:21:47 -0800

    Version 3.0.25

--- weatherflow.py
+++ weatherflow.py
@@ -15,7 +15,7 @@ LOGGER = udi_interface.LOGGER
 if __name__ == "__main__":
     try:
         polyglot = udi_interface.Interface([weatherflow.Controller])
-        polyglot.start('3.0.24')
+        polyglot.start('3.0.25')
         control = weatherflow.Controller(polyglot, 'controller', 'controller', 'WeatherFlow')
         polyglot.runForever()
     except (KeyboardInterrupt, SystemExit):
